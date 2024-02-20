55c22e952dfa3fbc9ca9249bb67e79dfbb8776f5
Andrey Borysenko <andrey18106x@gmail.com>
2024-02-20 16:21:59 +0200

    minor fixes, test translations for Files,Settings

--- lib/main.py
+++ lib/main.py
@@ -4,8 +4,9 @@ import locale
 import os
 import random
 from contextlib import asynccontextmanager
+from typing import Annotated
 
-from fastapi import FastAPI, responses
+from fastapi import FastAPI, responses, Header
 from pydantic import BaseModel
 
 from nc_py_api import NextcloudApp
@@ -16,6 +17,7 @@ from nc_py_api.ex_app import (
     SettingsField,
     SettingsFieldType,
     SettingsForm,
+    UiActionFileInfo
 )
 
 import gettext
@@ -23,15 +25,13 @@ import gettext
 # ../locale/<lang>/LC_MESSAGES/<app_id>.(mo|po)
 localedir = os.path.join(os.path.dirname(os.path.dirname(__file__)), "locale")
 locale.setlocale(locale.LC_ALL)
-# locale.bindtextdomain(os.getenv("APP_ID"), localedir)  - do we need this? It does not work on macOS..
 my_l10n = gettext.translation(
-    os.getenv("APP_ID"), localedir, fallback=True, languages=["en", "uk"]
+    os.getenv("APP_ID"), localedir, fallback=True, languages=["en"]  # English is always available and is the default
 )
 my_l10n.install()
 
 _ = my_l10n.gettext
 _n = my_l10n.ngettext
-
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -47,122 +47,122 @@ SETTINGS_EXAMPLE = SettingsForm(
     id="settings_example",
     section_type="admin",
     section_id="ai_integration_team",
-    title="Example of declarative settings",
-    description="These fields are rendered dynamically from declarative schema",
+    title=_("Example of declarative settings"),
+    description=_("These fields are rendered dynamically from declarative schema"),
     fields=[
         SettingsField(
             id="field1",
             title="Multi-selection",
-            description="Select some option setting",
+            description=_("Select some option setting"),
             type=SettingsFieldType.MULTI_SELECT,
             default=["foo", "bar"],
-            placeholder="Select some multiple options",
+            placeholder=_("Select some multiple options"),
             options=["foo", "bar", "baz"],
         ),
         SettingsField(
             id="some_real_setting",
-            title="Choose init status check background job interval",
-            description="How often ExApp should check for initialization status",
+            title=_("Choose init status check background job interval"),
+            description=_("How often ExApp should check for initialization status"),
             type=SettingsFieldType.RADIO,
             default="40m",
-            placeholder="Choose init status check background job interval",
+            placeholder=_("Choose init status check background job interval"),
             options={
-                "Each 40 minutes": "40m",
-                "Each 60 minutes": "60m",
-                "Each 120 minutes": "120m",
-                "Each day": f"{60 * 24}m",
+                _("Each 40 minutes"): "40m",
+                _("Each 60 minutes"): "60m",
+                _("Each 120 minutes"): "120m",
+                _("Each day"): f"{60 * 24}m",
             },
         ),
         SettingsField(
             id="test_ex_app_field_1",
-            title="Default text field",
-            description="Set some simple text setting",
+            title=_("Default text field"),
+            description=_("Set some simple text setting"),
             type=SettingsFieldType.TEXT,
             default="foo",
-            placeholder="Enter text setting",
+            placeholder=_("Enter text setting"),
         ),
         SettingsField(
             id="test_ex_app_field_1_1",
-            title="Email field",
-            description="Set email config",
+            title=_("Email field"),
+            description=_("Set email config"),
             type=SettingsFieldType.EMAIL,
             default="",
-            placeholder="Enter email",
+            placeholder=_("Enter email"),
         ),
         SettingsField(
             id="test_ex_app_field_1_2",
-            title="Tel field",
-            description="Set tel config",
+            title=_("Tel field"),
+            description=_("Set tel config"),
             type=SettingsFieldType.TEL,
             default="",
-            placeholder="Enter your tel",
+            placeholder=_("Enter your tel"),
         ),
         SettingsField(
             id="test_ex_app_field_1_3",
-            title="Url (website) field",
-            description="Set url config",
+            title=_("Url (website) field"),
+            description=_("Set url config"),
             type=SettingsFieldType.URL,
             default="",
-            placeholder="Enter url",
+            placeholder=_("Enter url"),
         ),
         SettingsField(
             id="test_ex_app_field_1_4",
-            title="Number field",
-            description="Set number config",
+            title=_("Number field"),
+            description=_("Set number config"),
             type=SettingsFieldType.NUMBER,
             default=0,
-            placeholder="Enter number value",
+            placeholder=_("Enter number value"),
         ),
         SettingsField(
             id="test_ex_app_field_2",
-            title="Password",
-            description="Set some secure value setting",
+            title=_("Password"),
+            description=_("Set some secure value setting"),
             type=SettingsFieldType.PASSWORD,
             default="",
-            placeholder="Set secure value",
+            placeholder=_("Set secure value"),
         ),
         SettingsField(
             id="test_ex_app_field_3",
-            title="Selection",
-            description="Select some option setting",
+            title=_("Selection"),
+            description=_("Select some option setting"),
             type=SettingsFieldType.SELECT,
             default="foo",
-            placeholder="Select some option setting",
+            placeholder=_("Select some option setting"),
             options=["foo", "bar", "baz"],
         ),
         SettingsField(
             id="test_ex_app_field_3",
-            title="Selection",
-            description="Select some option setting",
+            title=_("Selection"),
+            description=_("Select some option setting"),
             type=SettingsFieldType.SELECT,
             default="foo",
-            placeholder="Select some option setting",
+            placeholder=_("Select some option setting"),
             options=["foo", "bar", "baz"],
         ),
         SettingsField(
             id="test_ex_app_field_4",
-            title="Toggle something",
-            description="Select checkbox option setting",
+            title=_("Toggle something"),
+            description=_("Select checkbox option setting"),
             type=SettingsFieldType.CHECKBOX,
             default=False,
-            label="Verify something if enabled",
+            label=_("Verify something if enabled"),
         ),
         SettingsField(
             id="test_ex_app_field_5",
-            title="Multiple checkbox toggles, describing one setting",
-            description="Select checkbox option setting",
+            title=_("Multiple checkbox toggles, describing one setting"),
+            description=_("Select checkbox option setting"),
             type=SettingsFieldType.MULTI_CHECKBOX,
             default={"foo": True, "bar": True},
             options={"Foo": "foo", "Bar": "bar", "Baz": "baz", "Qux": "qux"},
         ),
         SettingsField(
             id="test_ex_app_field_6",
-            title="Radio toggles, describing one setting like single select",
-            description="Select radio option setting",
+            title=_("Radio toggles, describing one setting like single select"),
+            description=_("Select radio option setting"),
             type=SettingsFieldType.RADIO,
-            label="Select single toggle",
+            label=_("Select single toggle"),
             default="foo",
-            options={"First radio": "foo", "Second radio": "bar", "Third radie": "baz"},
+            options={_("First radio"): "foo", _("Second radio"): "bar", _("Third radio"): "baz"},
         ),
     ],
 )
@@ -179,6 +179,8 @@ def enabled_handler(enabled: bool, nc: NextcloudApp) -> str:
         )
         nc.ui.resources.set_script("top_menu", "first_menu", "js/ui_example-main")
         nc.ui.top_menu.register("first_menu", "UI example", "img/app.svg")
+        nc.ui.files_dropdown_menu.register("test_menu", _("Test menu"), "/test_menu", mime="image/jpeg", icon="img/app-dark.svg")
+
         if nc.srv_version["major"] >= 29:
             nc.ui.settings.register_form(SETTINGS_EXAMPLE)
     else:
@@ -187,6 +189,7 @@ def enabled_handler(enabled: bool, nc: NextcloudApp) -> str:
         )
         nc.ui.resources.delete_script("top_menu", "first_menu", "js/ui_example-main")
         nc.ui.top_menu.unregister("first_menu")
+        nc.ui.files_dropdown_menu.unregister("test_menu")
     return ""
 
 
@@ -202,6 +205,15 @@ async def verify_initial_value(
     return responses.JSONResponse(
         content={"initial_value": str(random.randint(0, 100))}, status_code=200
     )
+
+@APP.post("/test_menu")
+async def test_menu_handler(
+    file: UiActionFileInfo,
+    accept_language: Annotated[str | None, Header()] = None
+):
+    print(f'File: {file}')
+    print(f'Accept-Language: {accept_language}')
+    return responses.Response()
 
 
 class FileInfo(BaseModel):
